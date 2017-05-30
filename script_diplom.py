ab0b19f6d1e1c53f35db8604be4861ca69c34d42
Дудаков Сергей <sdudakov@corp.local>
2017-05-30 16:04:14 +0300

    первый вариант дипломной работы (после проверки PEP8)

--- script_diplom.py
+++ script_diplom.py
@@ -32,14 +32,17 @@ params = {'access_token': 'd13e692be69592b09fd22c77a590dd34e186e6d696daa88d6d981
 # функция возвращает список групп пользователя
 def groups_list(user_id):
     params['user_ids'] = user_id
-    response = requests.get('https://api.vk.com/method/groups.get', params) #список групп пользователя
+    # список групп пользователя
+    response = requests.get('https://api.vk.com/method/groups.get', params)
     groups = response.json()['response']['items']
     return groups
+
 
 # функция возвращает список друзей пользователя
 def frends_list(user_id):
     params['user_ids'] = user_id
-    response = requests.get('https://api.vk.com/method/friends.get', params) #список друзей
+    # список друзей
+    response = requests.get('https://api.vk.com/method/friends.get', params)
     frends = response.json()['response']['items']
     return frends
 
@@ -53,19 +56,25 @@ def list_to_string(items):
     return string
 
 
-# функция возвращает множество групп пользователя в которых не состоит ни один из его друзей
+# функция возвращает множество групп пользователя,
+# в которых не состоит ни один из его друзей
 def user_unique_groups(groups, frends):
     unique_groups = set()
     groups_count = len(groups)
     params['user_ids'] = list_to_string(frends)
     for group in groups:
         params['group_id'] = group
-        response = requests.get('https://api.vk.com/method/groups.isMember', params) #информация о том, является ли пользователь участником сообщества
+        # информация о том, является ли пользователь участником сообщества
+        response = requests.get('https://api.vk.com/method/groups.isMember',
+                                params
+                                )
         time.sleep(0.5)
         groups_count -= 1
         for item in response.json():
-            if not 'error' in item:
-                print('группа {}, осталось проверить {} групп из {}'.format(group, groups_count, len(groups)))
+            if 'error' not in item:
+                print('группа {}, осталось проверить {} групп из {}'
+                      .format(group, groups_count, len(groups))
+                      )
                 for item in response.json()['response']:
                     if item['member'] == 0:
                         unique_groups.add(group)
@@ -79,15 +88,20 @@ def write_groups_to_json(groups):
     params['fields'] = 'members_count'
     params['group_ids'] = list_to_string(groups)
     groups_list = []
-    response = requests.get('https://api.vk.com/method/groups.getById', params) #информация о группе
+    # информация о группе
+    response = requests.get('https://api.vk.com/method/groups.getById', params)
     for response in response.json()['response']:
-        if not 'deactivated' in response:
-            groups_dict = {'name': response['name'], 'gid': response['id'], 'members_count': response['members_count']}
+        if 'deactivated' not in response:
+            groups_dict = {'name': response['name'],
+                           'gid': response['id'],
+                           'members_count': response['members_count']
+                           }
             groups_list.append(groups_dict)
     print('Данные о группах пишем в файл groups.json')
     with open('groups.json', mode='w', encoding='utf-8') as file:
         json.dump(groups_list, file, ensure_ascii=False, indent=2)
     return 0
+
 
 user_id = 5030613
 # user_id = 'tim_leary'
