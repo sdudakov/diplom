16a108c0148028e2284be9c8f5cb3425f4c15299
Дудаков Сергей <sdudakov@corp.local>
2017-05-30 15:39:31 +0300

    первый вариант дипломной работы

--- script_diplom.py
+++ script_diplom.py
@@ -1,105 +1,97 @@
-# Задание: Вывести список групп в ВК в которых состоит пользователь, но не
-# состоит никто из его друзей. В качестве жертвы, на ком тестировать, можно
-# использовать: https://vk.com/tim_leary
+"""
+Задание: Вывести список групп в ВК в которых состоит пользователь, но не
+состоит никто из его друзей. В качестве жертвы, на ком тестировать, можно
+использовать: https://vk.com/tim_leary
 
-# Внимание: и имя пользователя (tim_leary) и id (5030613)  - являются
-# валидными входными данными
-# Ввод можно организовать любым способом:
-# из консоли
-# из параметров командной строки при запуске
-# из переменной
+Внимание: и имя пользователя (tim_leary) и id (5030613)  - являются
+валидными входными данными
+Ввод можно организовать любым способом:
+из консоли
+из параметров командной строки при запуске
+из переменной
 
-# Программа не падает, если один из друзей пользователя помечен как “удалён” или
-# “заблокирован”
-# Показывает что не зависла: рисует точку или чёрточку на каждое обращение к api
-# Не падает, если было слишком много обращений к API
-# (Too many requests per second)
-# Ограничение от ВК: не более 3х обращений к API в секунду.
-# Могут помочь модуль time (time.sleep) и конструкция (try/except)
-# Код программы удовлетворяет PEP8
+Программа не падает, если один из друзей пользователя помечен как “удалён” или
+“заблокирован”
+Показывает что не зависла: рисует точку или чёрточку на каждое обращение к api
+Не падает, если было слишком много обращений к API
+(Too many requests per second)
+Ограничение от ВК: не более 3х обращений к API в секунду.
+Могут помочь модуль time (time.sleep) и конструкция (try/except)
+Код программы удовлетворяет PEP8
+"""
 
-# 1. получить список групп пользователя и список его друзей
-# 2. для каждого друга получить список групп (можно объединитьих в множество groups_set() - что бы не было повторений)
-# 3. проверить для каждой группы пользователя есть ли такая же в этом множестве groups_set() если нет - это будет выходными данными
-# 4. либо функцией https://vk.com/dev/groups.isMember
-
-#import vk
 import requests
 import time
+import json
 
 params = {'access_token': 'd13e692be69592b09fd22c77a590dd34e186e6d696daa88d6d981e1b4e296b14acb377e82dcbc81dc0f22',
           'v': '5.62',
           }
-user_id = 5030613
-#user_id = 'tim_leary'
 
-#функция возвращает список групп пользователя
+
+# функция возвращает список групп пользователя
 def groups_list(user_id):
     params['user_ids'] = user_id
-    #response = requests.get('https://api.vk.com/method/users.get', params)  #информация о пользователе
-    #response = requests.get('https://api.vk.com/method/friends.get', params) #список друзей
     response = requests.get('https://api.vk.com/method/groups.get', params) #список групп пользователя
     groups = response.json()['response']['items']
     return groups
 
-#функция возвращает список друзей пользователя
+# функция возвращает список друзей пользователя
 def frends_list(user_id):
     params['user_ids'] = user_id
     response = requests.get('https://api.vk.com/method/friends.get', params) #список друзей
     frends = response.json()['response']['items']
     return frends
 
-#функция преобразует список друзей пользователя в строку
-def frends_list_to_string(frends):
-    frends_string = ''
-    for frend in frends:
-        frends_string += str(frend)
-        frends_string += ', '
-    return frends_string
 
-#функция возвращает множество groups_set() всех групп в которых состоят друзья - так делать не стал
-#проверить состоит ли друг в группе, если нет то добавить эту группу в список лист, если да то проверить есть ли группа в списке лист и удалить ее оттуда
-
-groups = groups_list(user_id)
-frends = frends_list(user_id)
+# функция преобразует список в строку
+def list_to_string(items):
+    string = ''
+    for item in items:
+        string += str(item)
+        string += ', '
+    return string
 
 
+# функция возвращает множество групп пользователя в которых не состоит ни один из его друзей
 def user_unique_groups(groups, frends):
-    unique_groups = []
+    unique_groups = set()
     groups_count = len(groups)
-    for group in groups:
-        params['group_id'] = group
-        frends_count = len(frends)
-        for frend in frends:
-            params['user_ids'] = frend
-            response = requests.get('https://api.vk.com/method/groups.isMember', params) #информация о том, является ли пользователь участником сообщества
-            print(response.json())
-            if response.json()['response'][0]['member'] == 0:
-                unique_groups.append(group)
-            else:
-                if group in unique_groups:
-                    unique_groups.remove(group)
-            time.sleep(0.25)
-            frends_count -= 1
-            print('fends_id {}, группа {}, осталось проверить {} друзей из {}'.format(frend, group, frends_count, len(frends)))
-        groups_count -= 1
-        print('осталось проверить {} групп из {}'.format(groups_count, len(groups)))
-    return unique_groups
-
-
-#print(user_unique_groups(groups, frends))
-
-
-def user_unique_groups(groups, frends):
-    unique_groups = []
-    groups_count = len(groups)
-    params['user_ids'] = frends_list_to_string(frends)
+    params['user_ids'] = list_to_string(frends)
     for group in groups:
         params['group_id'] = group
         response = requests.get('https://api.vk.com/method/groups.isMember', params) #информация о том, является ли пользователь участником сообщества
-        print(response.json())
-        time.sleep(0.25)
+        time.sleep(0.5)
         groups_count -= 1
-        print('группа {}, осталось проверить {} групп из {}'.format(group, groups_count, len(groups)))
+        for item in response.json():
+            if not 'error' in item:
+                print('группа {}, осталось проверить {} групп из {}'.format(group, groups_count, len(groups)))
+                for item in response.json()['response']:
+                    if item['member'] == 0:
+                        unique_groups.add(group)
+                    else:
+                        unique_groups.remove(group)
     return unique_groups
-print(user_unique_groups(groups, frends))
+
+
+# функция записывает информацию о группах в файл формата json
+def write_groups_to_json(groups):
+    params['fields'] = 'members_count'
+    params['group_ids'] = list_to_string(groups)
+    groups_list = []
+    response = requests.get('https://api.vk.com/method/groups.getById', params) #информация о группе
+    for response in response.json()['response']:
+        if not 'deactivated' in response:
+            groups_dict = {'name': response['name'], 'gid': response['id'], 'members_count': response['members_count']}
+            groups_list.append(groups_dict)
+    print('Данные о группах пишем в файл groups.json')
+    with open('groups.json', mode='w', encoding='utf-8') as file:
+        json.dump(groups_list, file, ensure_ascii=False, indent=2)
+    return 0
+
+user_id = 5030613
+# user_id = 'tim_leary'
+user_groups = groups_list(user_id)
+user_frends = frends_list(user_id)
+unique_groups = user_unique_groups(user_groups, user_frends)
+write_groups_to_json(unique_groups)
