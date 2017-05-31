fe15d3c245ef28c7989fe63a3edf2be4f631b44c
Дудаков Сергей <sdudakov@corp.local>
2017-05-31 17:23:48 +0300

    второй вариант дипломной работы (работа над замечаниями)

--- script_diplom.py
+++ script_diplom.py
@@ -31,20 +31,30 @@ params = {'access_token': 'd13e692be69592b09fd22c77a590dd34e186e6d696daa88d6d981
 
 # функция возвращает список групп пользователя
 def groups_list(user_id):
-    params['user_ids'] = user_id
+    params['user_id'] = user_id
     # список групп пользователя
     response = requests.get('https://api.vk.com/method/groups.get', params)
-    groups = response.json()['response']['items']
+    for item in response.json():
+        if 'error' not in item:
+            groups = response.json()['response']['items']
+        else:
+            print('у пользователя {} нет групп'.format(user_id))
+            exit(0)
     return groups
 
 
 # функция возвращает список друзей пользователя
-def frends_list(user_id):
-    params['user_ids'] = user_id
+def friends_list(user_id):
+    params['user_id'] = user_id
     # список друзей
     response = requests.get('https://api.vk.com/method/friends.get', params)
-    frends = response.json()['response']['items']
-    return frends
+    friends = response.json()['response']['items']
+    return friends
+
+
+# функция делит список друзей пользователя на части по 500 в каждой
+def divide_items_into_parts(items, part_size=500):
+    return [items[i:i+part_size] for i in range(0, len(items), part_size)]
 
 
 # функция преобразует список в строку
@@ -58,16 +68,14 @@ def list_to_string(items):
 
 # функция возвращает множество групп пользователя,
 # в которых не состоит ни один из его друзей
-def user_unique_groups(groups, frends):
+def unique_groups_set(groups, friends):
     unique_groups = set()
     groups_count = len(groups)
-    params['user_ids'] = list_to_string(frends)
+    params['user_ids'] = list_to_string(friends)
     for group in groups:
         params['group_id'] = group
         # информация о том, является ли пользователь участником сообщества
-        response = requests.get('https://api.vk.com/method/groups.isMember',
-                                params
-                                )
+        response = requests.post('https://api.vk.com/method/groups.isMember', params)
         time.sleep(0.5)
         groups_count -= 1
         for item in response.json():
@@ -79,7 +87,7 @@ def user_unique_groups(groups, frends):
                     if item['member'] == 0:
                         unique_groups.add(group)
                     else:
-                        unique_groups.remove(group)
+                        pass
     return unique_groups
 
 
@@ -89,7 +97,7 @@ def write_groups_to_json(groups):
     params['group_ids'] = list_to_string(groups)
     groups_list = []
     # информация о группе
-    response = requests.get('https://api.vk.com/method/groups.getById', params)
+    response = requests.post('https://api.vk.com/method/groups.getById', params)
     for response in response.json()['response']:
         if 'deactivated' not in response:
             groups_dict = {'name': response['name'],
@@ -103,9 +111,26 @@ def write_groups_to_json(groups):
     return 0
 
 
+# функция проверяет количество друзей у пользователя для соблюдения ограничений ВК
+def сheck_friends_number(user_groups, user_friends):
+    if len(user_friends) > 500:
+        divide_user_friends = divide_items_into_parts(user_friends)
+        count_friends = len(user_friends)
+        composite_unique_groups_set = set()
+        for item in divide_user_friends:
+            print('осталось проверить {} друзей из {}'.format(count_friends, len(user_friends)))
+            count_friends -= len(item)
+            for item in unique_groups_set(user_groups, item):
+                composite_unique_groups_set.add(item)
+    else:
+        composite_unique_groups_set = unique_groups_set(user_groups, user_friends)
+    return composite_unique_groups_set
+
+
 user_id = 5030613
 # user_id = 'tim_leary'
+
 user_groups = groups_list(user_id)
-user_frends = frends_list(user_id)
-unique_groups = user_unique_groups(user_groups, user_frends)
+user_friends = friends_list(user_id)
+unique_groups = сheck_friends_number(user_groups, user_friends)
 write_groups_to_json(unique_groups)
